e91290a2ae586a54682d417cc2ec067606be9fdf
Ajax Lima <ajaxlima94@gmail.com>
2024-08-06 13:46:25 -0300

    feat: adicao do equals e hashCode

--- src/main/java/com/cars/hot/api/carro/entity/Carro.java
+++ src/main/java/com/cars/hot/api/carro/entity/Carro.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Objects;
+
 @Entity
 @Getter
 @Setter
@@ -27,5 +29,18 @@ public class Carro {
                 ", serie='" + serie + '\'' +
                 ", numeroSerie=" + numeroSerie +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Carro carro = (Carro) o;
+        return Objects.equals(id, carro.id) && Objects.equals(modelo, carro.modelo) && Objects.equals(serie, carro.serie) && Objects.equals(numeroSerie, carro.numeroSerie);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, modelo, serie, numeroSerie);
     }
 }
